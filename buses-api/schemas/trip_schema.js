f6a732088fec90e3b22522cfcb1281c3c524fbc0
Luciano Palacio <lucianopalacio@toptive.co>
2022-06-17 16:05:04 -0300

    Add times and model cities for trip schema

--- buses-api/schemas/trip_schema.js
+++ buses-api/schemas/trip_schema.js
@@ -1,20 +1,31 @@
 var mongoose = require('mongoose');
 var Passenger = require('./passenger_schema.js').schema;
+var City = require('./city_schema.js').schema;
 
 var Schema = mongoose.Schema;
 
 var trip = new Schema({
     departure_city: {
-        type: String,
-        default: "",
-    }, 
+        type: Schema.Types.ObjectId,
+        ref: 'City',
+        es_schema: City,
+    },
     arrival_city: {
-        type: String,
-        default: "",
+        type: Schema.Types.ObjectId,
+        ref: 'City',
+        es_schema: City,
     },
     cant_passengers_total: {
         type: Number,
         default: 0,
+    },
+    departure_time: {
+        type: Date,
+        default: null,
+    },
+    arrival_time: {
+        type: Date,
+        default: null,
     },
     passengers: [{
         type: Schema.Types.ObjectId,
