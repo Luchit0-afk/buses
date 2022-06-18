cafdc60df82bd3f0c128e1f961f734067ca59bd9
Luciano Palacio <lucianopalacio@toptive.co>
2022-06-18 16:42:37 -0300

    Initial changes for a main page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import FormPassenger from './../components/FormPassenger.js'
 import { getAllCities } from './../services/main.js';
+import { Image, Button } from 'antd';
 
 export default function Home({ cities }) {
   return (
@@ -12,17 +13,49 @@ export default function Home({ cities }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="#">Buses App</a>
+      <main>
+        <h1 >
+          Welcome to Buses App
         </h1>
-      </main>  */}
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed sit amet pulvinar ex. Aenean molestie interdum quam.
+          Praesent nec massa eget nulla auctor aliquam. 
+          Curabitur auctor luctus tellus ac lobortis.
+        </h3>
+        <div class="home-buses-images">
+          <Image
+            width={200}
+            height={200}
+            src="images/bus1.jpg"
+          />
+          <Image
+            width={200}
+            height={200}
+            src="images/bus2.jpg"
+          />
+          <Image
+            width={200}
+            height={200}
+            src="images/bus3.jpg"
+          />
+          <Image
+            width={200}
+            height={200}
+            src="images/bus4.jpg"
+          />
+        </div>
+        <div class="home-buttons">
+          <Button type="primary">
+            Buy a Ticket
+          </Button>
+        </div>
+        <footer>
+          <p>Test Footer</p>
+        </footer>
+      </main> 
 
-      <FormPassenger cities={cities}/>
-
-      <footer className={styles.footer}>
-        Test Footer
-      </footer>
+      {/* <FormPassenger cities={cities}/> */}
     </div>
   )
 }
