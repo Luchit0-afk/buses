5cc18a69544843f13f381bf572475457125ac10f
Luciano Palacio <lucianopalacio@toptive.co>
2022-06-24 16:09:20 -0300

    Add a login with parameters

--- buses-api/app.js
+++ buses-api/app.js
@@ -1,5 +1,6 @@
 //Require the data base configurations
 require('./config/db_connection.js');
+require('./config/passport.js');
 
 var createError = require('http-errors');
 var express = require('express');
@@ -32,15 +33,10 @@ liveReloadServer.server.once("connection", () => {
 app.use(connectLiveReload());
 
 //Midlewares
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug')
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'ultra-secret',
   resave: true,
@@ -49,25 +45,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new PassportLocal( function(username, password, done){
-  //Aca debemos buscar en la base de datos el username y la contraseña para verificar que los datos esten en la base de datos
-  if(username === "test" && password === "12345"){
-    return done(null, { id: 1, username: "test" });
-  }
-  done(null, { success: false });
-  //CREO QUE NUNCA DEVOLVEMOS EL ERROR SI HUBO ALGUNO
-}))
-
-//Serializacion
-passport.serializeUser(function(user, done){
-  done(null, user.id);
-})
-
-//Deserializacion
-passport.deserializeUser(function(id, done){
-  done(null, { id: 1, username: "Luciano" });
-})
-
 //Routes
 app.use('/credit_card', CreditCard);
 app.use('/trip', Trip);
@@ -75,6 +52,8 @@ app.use('/city', City);
 app.use('/passenger', Passenger);
 app.use('/user', User);
 
+
+//Mejorar la captura del error 404
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- buses-api/routes/user/user.js
+++ buses-api/routes/user/user.js
@@ -3,7 +3,7 @@ var User = require('./../../schemas/user_schema.js');
 
 module.exports.register = async function (req, res) {
     try {
-        const { email, password } = req.body;
+        const { username, email, password } = req.body;
         const user = await User.create({
             username,
             email,
@@ -20,11 +20,15 @@ module.exports.login = async function (req, res) {
     passport.authenticate('local', function(err, user, info) {
         if (err) {
           console.log('passport error exception');
-          res.status(401).json(err);
+          res.status(401).json({ success: false, err });
           return;
         }
         console.log(user);
-        console.log(info);
-        res.status(200).json({ success: true });
+        if( !!user ){
+            res.status(200).json({ success: true, user });
+        }
+        else{
+            res.status(200).json({ success: false });
+        }
     })(req, res);
 }
